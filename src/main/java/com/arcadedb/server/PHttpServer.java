0b4db716c9e85ec245440e6a264f9f05c1bbcba0
robfrank <ro.franchini@gmail.com>
2018-04-29 17:44:30 +0200

    adds docker packaging

--- src/main/java/com/arcadedb/server/PHttpServer.java
+++ src/main/java/com/arcadedb/server/PHttpServer.java
@@ -15,52 +15,53 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class PHttpServer {
-  private       Undertow                         server;
-  private       ConcurrentMap<String, PDatabase> databases      = new ConcurrentHashMap<>();
-  private       PJsonSerializer                  jsonSerializer = new PJsonSerializer();
-  private final PHttpServerConfiguration         configuration;
+    private final PHttpServerConfiguration configuration;
+    private Undertow server;
+    private ConcurrentMap<String, PDatabase> databases = new ConcurrentHashMap<>();
+    private PJsonSerializer jsonSerializer = new PJsonSerializer();
 
-  public PHttpServer(final PHttpServerConfiguration configuration) {
-    this.configuration = configuration;
+    public PHttpServer(final PHttpServerConfiguration configuration) {
+        this.configuration = configuration;
 
-    final HttpHandler routes = new RoutingHandler().get("/query/{database}/{text}", new PQueryHandler(this))
-        .get("/record/{database}/{rid}", new PRecordHandler(this));
-    server = Undertow.builder().addHttpListener(configuration.bindPort, configuration.bindServer).setHandler(routes).build();
-  }
-
-  public void close() {
-    PLogManager.instance().info(this, "Shutting down server...");
-    server.stop();
-    for (PDatabase db : databases.values())
-      db.close();
-    PLogManager.instance().info(this, "The server is down");
-  }
-
-  public PJsonSerializer getJsonSerializer() {
-    return jsonSerializer;
-  }
-
-  public synchronized PDatabase getDatabase(final String databaseName) {
-    PDatabase db = databases.get(databaseName);
-    if (db == null) {
-      db = new PDatabaseFactory(configuration.databaseDirectory + "/" + databaseName, PPaginatedFile.MODE.READ_WRITE).acquire();
-
-      final PDatabase oldDb = databases.putIfAbsent(databaseName, db);
-
-      if (oldDb != null)
-        db = oldDb;
+        final HttpHandler routes = new RoutingHandler()
+                .get("/query/{database}/{text}", new PQueryHandler(this))
+                .get("/record/{database}/{rid}", new PRecordHandler(this));
+        server = Undertow.builder().addHttpListener(configuration.bindPort, configuration.bindServer).setHandler(routes).build();
     }
 
-    return db;
-  }
+    public static void main(final String[] args) {
+        new PHttpServer(PHttpServerConfiguration.create()).run();
+    }
 
-  public void run() {
-    PLogManager.instance()
-        .info(this, "Starting ArcadeDB server (server=%s port=%d)...", configuration.bindServer, configuration.bindPort);
-    server.start();
-  }
+    public void close() {
+        PLogManager.instance().info(this, "Shutting down server...");
+        server.stop();
+        for (PDatabase db : databases.values())
+            db.close();
+        PLogManager.instance().info(this, "The server is down");
+    }
 
-  public static void main(final String[] args) {
-    new PHttpServer(PHttpServerConfiguration.create()).run();
-  }
+    public PJsonSerializer getJsonSerializer() {
+        return jsonSerializer;
+    }
+
+    public synchronized PDatabase getDatabase(final String databaseName) {
+        PDatabase db = databases.get(databaseName);
+        if (db == null) {
+            db = new PDatabaseFactory(configuration.databaseDirectory + "/" + databaseName, PPaginatedFile.MODE.READ_WRITE).acquire();
+
+            final PDatabase oldDb = databases.putIfAbsent(databaseName, db);
+
+            if (oldDb != null)
+                db = oldDb;
+        }
+
+        return db;
+    }
+
+    public void run() {
+        PLogManager.instance()
+                .info(this, "Starting ArcadeDB server (server=%s port=%d)...", configuration.bindServer, configuration.bindPort);
+        server.start();
+    }
 }
