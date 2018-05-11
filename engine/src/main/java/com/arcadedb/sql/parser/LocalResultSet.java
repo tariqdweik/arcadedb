3ca37c5e5417918650ee65bc446b1689389d8b2b
lvca <l.garulli@gmail.com>
2018-05-11 09:09:07 +0200

    Mnaged RecordNotFoundException during browsing

--- engine/src/main/java/com/arcadedb/sql/parser/LocalResultSet.java
+++ engine/src/main/java/com/arcadedb/sql/parser/LocalResultSet.java
@@ -4,6 +4,7 @@
 
 package com.arcadedb.sql.parser;
 
+import com.arcadedb.exception.RecordNotFoundException;
 import com.arcadedb.sql.executor.ExecutionPlan;
 import com.arcadedb.sql.executor.InternalExecutionPlan;
 import com.arcadedb.sql.executor.Result;
@@ -28,46 +29,59 @@ public class LocalResultSet implements ResultSet {
   }
 
   private boolean fetchNext() {
-    lastFetch = executionPlan.fetchNext(100000);
-    if (!lastFetch.hasNext()) {
-      finished = true;
-      return false;
-    }
-    return true;
-  }
+    do {
+      try {
+        lastFetch = executionPlan.fetchNext(100000);
+        if (!lastFetch.hasNext()) {
+          finished = true;
+          return false;
+        }
 
-  @Override public boolean hasNext() {
-    if (finished) {
-      return false;
-    }
-    if (lastFetch.hasNext()) {
-      return true;
-    } else {
-      return fetchNext();
-    }
-  }
+        return true;
 
-  @Override public Result next() {
-    if (finished) {
-      throw new IllegalStateException();
-    }
-    if (!lastFetch.hasNext()) {
-      if (!fetchNext()) {
-        throw new IllegalStateException();
+      } catch (RecordNotFoundException e) {
+        // IGNORE, FETCH NEXT
       }
+    } while (true);
+  }
+
+  @Override
+  public boolean hasNext() {
+    if (finished)
+      return false;
+
+    if (lastFetch.hasNext())
+      return true;
+    else
+      return fetchNext();
+
+  }
+
+  @Override
+  public Result next() {
+    if (finished)
+      throw new IllegalStateException();
+
+    if (!lastFetch.hasNext()) {
+      if (!fetchNext())
+        throw new IllegalStateException();
+
     }
     return lastFetch.next();
   }
 
-  @Override public void close() {
+  @Override
+  public void close() {
     executionPlan.close();
   }
 
-  @Override public Optional<ExecutionPlan> getExecutionPlan() {
+  @Override
+  public Optional<ExecutionPlan> getExecutionPlan() {
     return Optional.of(executionPlan);
   }
 
-  @Override public Map<String, Long> getQueryStats() {
+  @Override
+  public Map<String, Long> getQueryStats() {
     return new HashMap<>();//TODO
   }
 
