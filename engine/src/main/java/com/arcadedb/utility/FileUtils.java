a2aaf3898775e47b14a6126591d61ad4f3f5e1d9
lvca <lvca@users.noreply.github.com>
2020-05-20 11:52:48 -0500

    Added escape of HTML method

--- engine/src/main/java/com/arcadedb/utility/FileUtils.java
+++ engine/src/main/java/com/arcadedb/utility/FileUtils.java
@@ -390,4 +390,19 @@ public class FileUtils {
       return sb.toString();
     }
   }
+
+  public static String escapeHTML(final String s) {
+    final StringBuilder out = new StringBuilder(Math.max(16, s.length()));
+    for (int i = 0; i < s.length(); i++) {
+      final char c = s.charAt(i);
+      if (c > 127 || c == '"' || c == '\'' || c == '<' || c == '>' || c == '&') {
+        out.append("&#");
+        out.append((int) c);
+        out.append(';');
+      } else {
+        out.append(c);
+      }
+    }
+    return out.toString();
+  }
 }
